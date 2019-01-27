83f5c114483c5bb6149ac4069699dae2468df32c
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 03:57:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ end
 def count_elements(array)
 #[{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
 #expected: [{:count=>2, :name=>"blake"}, {:count=>1, :name=>"ashley"}]
-  array.count { |element| name += 1 }
+
 end
 
 
