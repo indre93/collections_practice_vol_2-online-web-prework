9cadc3d2588973cfd842d5781ed7df31714043dc
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 02:30:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ end
 def count_elements(array)
 #[{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
 #expected: [{:count=>2, :name=>"blake"}, {:count=>1, :name=>"ashley"}]
-  array[1].insert(0, :count)
+  array[1][2]
 end
 
 
