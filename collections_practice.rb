d4774d70483c6e718affd70a888d685dbd24904d
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 04:01:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,6 +54,6 @@ def organize_schools(schools)
 #     "Chicago"=>["dev boot camp chicago"]}
 #  }
   schools.invert.each do |key, value|
-    puts "#{key.sort.to_i}: #{value.to_a}"
+    puts "#{key.sort.to_s}: [#{value}]"
   end
 end
