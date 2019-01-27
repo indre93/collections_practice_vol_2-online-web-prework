0e8d18b4c47fcdd0f5bd0760ffb3f206624a7698
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 02:37:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ end
 def count_elements(array)
 #[{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
 #expected: [{:count=>2, :name=>"blake"}, {:count=>1, :name=>"ashley"}]
-  new_array(:count) = array.count
+  new_array = array[0]
   array.map {|x| new_array << x}
 end
 
